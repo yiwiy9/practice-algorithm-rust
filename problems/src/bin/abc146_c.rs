19cc2296f3a44b41503c6a0e3547ef0fd4cbed8c
yiwiy9 <yiwiy9@gmail.com>
2023-07-01 14:20:05 +0900

    Add another solution of abc146/c

--- problems/src/bin/abc146_c.rs
+++ problems/src/bin/abc146_c.rs
@@ -1,5 +1,4 @@
 use proconio::input;
-use std::cmp::Ordering;
 
 fn main() {
     input! {
@@ -8,26 +7,53 @@ fn main() {
         x: i64,
     }
 
-    let mut ans = 0;
-    for d in 1..=18 {
-        let n = (x - (b * d)) / a;
-        if n <= 0 {
-            break;
-        }
+    let d = |n: i64| n.to_string().len() as i64;
+    let f = |n: i64| a * n + b * d(n);
 
-        let mut num = n;
-        let mut num_d = 0;
-        while num != 0 {
-            num /= 10;
-            num_d += 1;
-        }
-
-        match num_d.cmp(&d) {
-            Ordering::Greater => ans = 10_i64.pow(d as u32) - 1,
-            Ordering::Less => break,
-            Ordering::Equal => ans = n,
+    let mut left = 0;
+    let mut right = 1_000_000_000 + 1;
+    while right - left > 1 {
+        let mid = (left + right) / 2;
+        if f(mid) <= x {
+            left = mid;
+        } else {
+            right = mid;
         }
     }
 
-    println!("{}", ans.min(1_000_000_000));
+    println!("{}", left);
 }
+
+// use proconio::input;
+// use std::cmp::Ordering;
+
+// fn main() {
+//     input! {
+//         a: i64,
+//         b: i64,
+//         x: i64,
+//     }
+
+//     let mut ans = 0;
+//     for d in 1..=18 {
+//         let n = (x - (b * d)) / a;
+//         if n <= 0 {
+//             break;
+//         }
+
+//         let mut num = n;
+//         let mut num_d = 0;
+//         while num != 0 {
+//             num /= 10;
+//             num_d += 1;
+//         }
+
+//         match num_d.cmp(&d) {
+//             Ordering::Greater => ans = 10_i64.pow(d as u32) - 1,
+//             Ordering::Less => break,
+//             Ordering::Equal => ans = n,
+//         }
+//     }
+
+//     println!("{}", ans.min(1_000_000_000));
+// }
