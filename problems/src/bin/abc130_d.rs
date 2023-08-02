334c97a22cc710cf19865802479797f7039d5c27
yiwiy9 <yiwiy9@gmail.com>
2023-08-03 01:09:29 +0900

    Add ABC130/D

---
+++ problems/src/bin/abc130_d.rs
@@ -0,0 +1,28 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n],
+    }
+
+    let mut ans = 0;
+    let mut j = 0;
+    let mut sum = 0;
+    for &a_i in &a {
+        while j < n {
+            if sum >= k {
+                break;
+            }
+            sum += a[j];
+            j += 1;
+        }
+        if sum >= k {
+            ans += n - j + 1;
+        }
+        sum -= a_i;
+    }
+
+    println!("{}", ans);
+}
